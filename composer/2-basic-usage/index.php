e957a39745203de1aaa5acf2b9efd0c703d3b746
felagund18 <felagund18@yahoo.com>
2016-05-24 05:52:38 +0000

    Comments are added into file index.php

--- composer/2-basic-usage/index.php
+++ composer/2-basic-usage/index.php
@@ -6,14 +6,14 @@ $faker = \Faker\Factory::create();
 
 $data = [];
 for ($i = 0; $i < 100; $i++) {
-    $age = $faker->numberBetween(20, 40);
+    $age = $faker->numberBetween(20, 40); //20 - 40 хооронд random(санамсаргүй) тоо үүсгэх
     $data[$i] = [
-        'id' => $faker->randomNumber(6),
+        'id' => $faker->randomNumber(6), //6н оронтой random тоо үүсгэх
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'age' => $age,
         'job' => $faker->jobTitle,
-        'birthday' => date('Y-m-d', strtotime('-'.$age.' years')),
+        'birthday' => date('Y-m-d', strtotime('-'.$age.' years')), //date('now') функийн утгаас $age жил хасах
         'email' => $faker->email,
         'phone_number' => $faker->phoneNumber,
     ];
