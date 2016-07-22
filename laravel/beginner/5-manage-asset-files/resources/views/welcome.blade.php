d089f79dab4b141d1f02096f0c8631f6483edbf7
felagund18 <felagund18@yahoo.com>
2016-07-22 11:16:31 +0000

    v0.0.10 Added Laravel Beginner 5-manage-asset-files Added Laravel Custom - Blog Part1 Added Laravel Custom - Blog Part2 Added Laravel Custom - Blog Part3

---
+++ laravel/beginner/5-manage-asset-files/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" />
+</head>
+<body>
+    Hello this is css test
+    
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
