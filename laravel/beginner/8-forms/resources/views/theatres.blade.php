09a1317fa5d20cbd9e8747e3b0fad5a7e29ad1af
felagund18 <felagund18@yahoo.com>
2016-07-28 15:30:15 +0000

    v0.0.16 Added Laravel Beginner - 8. Forms

---
+++ laravel/beginner/8-forms/resources/views/theatres.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    @foreach ($theatres as $item)
+        <div>{{ $item->name }}</div>
+    @endforeach
+    <hr>
+    
+    <form method="post" action="{{ url('theatre/store') }}">
+        <div class="form-group">
+            <label>Кино театр</label>
+            <input class="form-control" type="text" name="name"/>
+        </div>
+        
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="submit" class="btn btn-default" value="Хадгалах"/>
+    </form>
+@endsection
