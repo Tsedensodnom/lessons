3076cc66fc41286e973af7871a8b9771218f375a
felagund18 <felagund18@yahoo.com>
2016-09-14 10:33:50 +0000

    v1.0.5
    Added Laravel Intermediate - Messenger Bot

---
+++ laravel/intermediate/4-newsbot/laravel/database/migrations/2016_09_11_113402_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('collection');
+            $table->text('title');
+            $table->text('url');
+            $table->text('image');
+            $table->string('date')->nullable()->default(null);
+            $table->integer('comment_count')->nullable()->default(0);
+            $table->integer('view_count')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
