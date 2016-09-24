6688dd987b234e6a722ba4fa509b36f60ecba656
felagund18 <felagund18@yahoo.com>
2016-09-24 13:03:38 +0000

    v1.0.8 Added Laravel User Activation

---
+++ laravel/intermediate/5-mailactivation/app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \App\User::creating(function ($user) {
+            $user->is_activated = 0;
+            $user->activation_code = \Hash::make(str_random(40));
+            
+            \Notification::send($user, new ActivationCodeRequested);
+            
+            return true;
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
